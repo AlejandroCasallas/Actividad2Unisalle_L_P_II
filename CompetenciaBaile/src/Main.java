9550081eb73cc6b2503ee6361c3e0fbbf1e80fed
Andriely Casallas <casallas17032007@gmail.com>
2025-08-24 18:31:04 -0500

    Create all the functions and all functional

--- CompetenciaBaile/src/Main.java
+++ CompetenciaBaile/src/Main.java
@@ -2,6 +2,8 @@
  * Programa que simula un concurso de baile donde cuatro jueces califican a los participantes.
  * El programa calcula el puntaje final de cada participante excluyendo la calificación más baja.
  * 
+ * 
+ 
  * Realizado con fines educativos para la universidad La Salle.
  */
 
